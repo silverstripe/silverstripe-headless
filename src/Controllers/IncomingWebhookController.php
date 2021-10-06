e1820e9594ecebfa2c3958e15b6783ce5375b71c
Aaron Carlino <unclecheese@leftandmain.com>
2021-10-07 11:08:49 +1300

    Fix overwrite items bug

--- src/Controllers/IncomingWebhookController.php
+++ src/Controllers/IncomingWebhookController.php
@@ -53,9 +53,8 @@ class IncomingWebhookController extends Controller
             ->first();
         $queued = $events->filter('Status', PublishEvent::STATUS_QUEUED)
             ->first();
-        
+
         $duration = $progress ? (time() - $progress->obj('Created')->getTimestamp()) : 0;
-        $queued->Items()->setByIDList(PublishQueueItem::getQueued()->column());
 
         switch ($event) {
             case IncomingWebhook::EVENT_DEPLOY_START:
